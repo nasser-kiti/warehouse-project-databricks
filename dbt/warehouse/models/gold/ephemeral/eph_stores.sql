f926c41b7a0397e8b8a9c497948a1bab1b7b2a29
Nasser <kiti.nasser@gmail.com>
2026-09-14 15:24:54 +0300

    feat(gold): Create and load gold layer

---
+++ dbt/warehouse/models/gold/ephemeral/eph_stores.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT
+    store_id,
+    store_name,
+    store_city,
+    store_province,
+    store_country,
+    store_created_timestamp,
+    store_updated_timestamp,
+    store_is_active,
+    store_processed_at,
+    CURRENT_TIMESTAMP() AS eph_store_processed_at
+FROM
+    {{ ref('obt_b') }}
